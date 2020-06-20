ecd703f87867e3b3bb8811c4098ce34bf09a8963
yuyufru <y3ff2638.aad223@gmail.com>
2020-06-20 16:43:42 +0900

    change7
    change7

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,18 +1,22 @@
-# config valid for current version and patch releases of Capistrano
-lock "~> 3.14.1"
+# config valid only for current version of Capistrano
+# capistranoのバージョンを記載。固定のバージョンを利用し続け、バージョン変更によるトラブルを防止する
+lock '3.14.1'
 
-set :application, "chat-space"
+# Capistranoのログの表示に利用する
+set :application, 'chat-space'
+
+# どのリポジトリからアプリをpullするかを指定する
 set :repo_url,  'git@github.com:yuyufru/chat-space.git'
 
 # バージョンが変わっても共通で参照するディレクトリを指定
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 
 set :rbenv_type, :user
-set :rbenv_ruby, '2,6,5' #カリキュラム通りに進めた場合、2.6.5です
+set :rbenv_ruby, '2.6.5' #カリキュラム通りに進めた場合、2.6.5です
 
 # どの公開鍵を利用してデプロイするか
 set :ssh_options, auth_methods: ['publickey'],
-                  keys: ['：~/.ssh/chat-space.pem'] 
+                  keys: ['~/.ssh/chat-space.pem'] 
 
 # プロセス番号を記載したファイルの場所
 set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
